7ff9a6659a5168efd81b02a0154865d6b2917fde
Callum <callumnewsome100@gmail.com>
2021-09-25 16:00:20 +0100

    Interaction model

---
+++ src/models/Interaction.js
@@ -0,0 +1,9 @@
+const { DataTypes } = require('sequelize');
+
+const interactionSchema = {
+    id:            { type: DataTypes.STRING, notNull: true, primaryKey: true },
+    tickets:       { type: DataTypes.JSON, notNull: true, defaultValue: {} },
+};
+
+
+module.exports = { name: 'interaction', schema: interactionSchema }
