49ec35e53ea3202dd6742ad10d07e76019751416
Sam <samennis127@gmail.com>
2021-09-28 01:54:45 +0100

    fix boost react schema

--- src/models/BoostReact.js
+++ src/models/BoostReact.js
@@ -2,7 +2,8 @@ const { DataTypes } = require('sequelize');
 
 const boostReactSchema = {
     id:            { type: DataTypes.STRING, notNull: true, primaryKey: true },
-    reaction:      { type: DataTypes.STRING, notNull: true, defaultValue: 0 },
+    reaction:      { type: DataTypes.STRING, notNull: true },
+    word:          { type: DataTypes.STRING, notNull: true },
 };
 
 
