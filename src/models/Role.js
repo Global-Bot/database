41d85f69bebc5ef7f1209cb618d76c75ec196fdd
Callum <callumnewsome100@gmail.com>
2021-11-17 13:31:16 +0000

    Add inUse column to role model

--- src/models/Role.js
+++ src/models/Role.js
@@ -38,6 +38,10 @@ function roleSchema(db) {
                 min: 0,
                 isNumeric: true,
             }
+        },
+        inUse: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
         }
     }
 }
