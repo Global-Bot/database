17d0b65d9f8e52e84e2f1d9a94709d20dfbf74cd
Callum <callumnewsome100@gmail.com>
2021-12-24 13:52:03 +0000

    Shop model

---
+++ src/models/Shop.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require('sequelize');
+
+function shopSchema(db) {
+    const itemTypes = db._options?.itemTypes;
+    if (!itemTypes || !Array.isArray(itemTypes)) throw new Error('Missing itemTypes array property');
+    
+    return {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        itemID: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [ itemTypes ],
+            }
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                max: Number.MAX_SAFE_INTEGER,
+                min: 0,
+                isNumeric: true,
+            }
+        }
+    };
+}
+
+function shopHooks(db) {
+    async function beforeCreate(instance) {
+        const { itemID, type } = instance;
+
+        const model = db.models[type];
+        if (!model) throw new Error(`Unable to find model from item type "${type}"`);
+
+        const existingItem = await model.findOne({ where: { id: itemID } });
+        if (!existingItem) throw new Error(`Unable to find the ${itemID} ${type}`);
+    }
+
+    async function beforeBulkUpdate(instance) {
+        return await beforeCreate(instance.where)
+    }
+
+    return { beforeCreate, beforeBulkUpdate };
+}
+
+
+module.exports = { name: 'shop', schema: shopSchema, hooks: shopHooks }
